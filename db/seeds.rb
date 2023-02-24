c384b063280ffd1d471793244d3f6f4d3558d535
Jhonel Villanueva <vvjhonel@gmail.com>
2023-02-24 22:07:46 +0800

    hotfix: Fix seeds for better examples
    Previously, some airports don't have any flights available. This commit
    will create 1 - 3 flights for each departure-arrival pair

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,63 +13,44 @@ Airport.delete_all
 puts "Tables deleted"
 
 airports = Airport.create([
-  { code: "BCD", name: "Bacolod-Silay Airport" },
   { code: "DRP", name: "Bicol International Airport" },
   { code: "TAG", name: "Bohol-Panglao International Airport " },
   { code: "BXU", name: "Bancasi Airport" },
   { code: "CYZ", name: "Cauayan Airport" },
   { code: "CBO", name: "Cotabato Airport" },
-  { code: "TAC", name: "Daniel Z. Romualdez Airport" },
   { code: "DPL", name: "Dipolog Airport" },
   { code: "DGT", name: "Dumaguete-Sibulan Airport" },
   { code: "GES", name: "General Santos International Airport" },
-  { code: "MPH", name: "Godofredo P. Ramos Airport" },
   { code: "OZC", name: "Labo Airport" },
   { code: "CGY", name: "Laguindingan Airport" },
-  { code: "WNP", name: "Naga Airport" },
   { code: "PAG", name: "Pagadian Airport" },
   { code: "RXS", name: "Roxas Airport" },
-  { code: "TWT", name: "Sanga-Sanga Airport" },
-  { code: "SJI", name: "San Jose Airport" },
   { code: "SFS", name: "Subic Bay International Airport" },
   { code: "TUG", name: "Tuguegarao Airport" },
   { code: "ZAM", name: "Zamboanga International Airport" },
   { code: "BSO", name: "Basco Airport" },
   { code: "CYP", name: "Calbayog Airport" },
   { code: "CGM", name: "Camiguin Airport" },
-  { code: "CRM", name: "Catarman National Airport" },
-  { code: "CYU", name: "Cuyo Airport" },
-  { code: "EUQ", name: "Evelio B. Javier Airport" },
-  { code: "USU", name: "Francisco B. Reyes Airport" },
-  { code: "JOL", name: "Jolo Airport" },
-  { code: "BAG", name: "Loakan Airport" },
-  { code: "MRQ", name: "Marinduque Airport" },
-  { code: "MBT", name: "Moises R. Espinosa Airport" },
   { code: "OMC", name: "Ormoc Airport" },
-  { code: "SGL", name: "Sangley Point Airport" },
-  { code: "SWL", name: "San Vicente Airport" },
-  { code: "IAO", name: "Sayak Airport" },
-  { code: "SUG", name: "Surigao Airport" },
-  { code: "TDG", name: "Tandag Airport" },
-  { code: "TBH", name: "Tugdan Airport" },
-  { code: "VRC", name: "Virac Airport" },
+  { code: "SUG", name: "Surigao Airport" }
 ])
 
 airport_ids = Airport.ids
-100.times do |i|
-  departure = rand(airport_ids[0]..airport_ids[-1])
-  arrival = nil
-  loop do
-    arrival = rand(airport_ids[0]..airport_ids[-1])
-    break unless departure == arrival
+airport_ids.each do |departure|
+  rand(1..3).times do
+    airport_ids.each do |arrival|
+      next if arrival == departure
+  
+      rand(1..3).times do |i|
+        Flight.create(
+              departure_airport_id: departure,
+              arrival_airport_id: arrival,
+              date: DateTime.new(2023, 2, Time.now.day + i, rand(0..23), 0, 0),
+              duration: rand(1..3)
+            )
+        
+        puts "Flight created"
+      end
+    end
   end
-
-  Flight.create(
-    departure_airport_id: departure,
-    arrival_airport_id: arrival,
-    date: Time.now + rand(0..5).day,
-    duration: rand(1..3)
-  )
-
-  puts "Flight created!"
 end
