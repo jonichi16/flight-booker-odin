c8080ded6f0a5c24d8584686b3f002d6cdb2936f
Jhonel Villanueva <vvjhonel@gmail.com>
2023-02-26 04:24:05 +0800

    Create the search result
    This commit will do the following:
    - Create query of the selected flight and show the result
    - Create a search result form with radio button for each flight
    
    Ticket id: TSK-15

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.order(:code).map { |airport| [ airport.code, airport.code ] }
+    @flights = Flight.pick_flight(params[:departure_code], params[:arrival_code], params[:date]).includes(:departure_airport, :arrival_airport)
+    @default_selected_flight = @flights.first
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,19 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  private 
+
+  def self.pick_flight(departure_code, arrival_code, flight_date)
+    if flight_date.nil?
+      flight_date = Date.today
+    else
+      flight_date = Date.new(flight_date["year"].to_i, flight_date["month"].to_i, flight_date["day"].to_i)
+    end
+
+    self.where("departure_airport_id=?", Airport.find_by_code(departure_code))
+        .where("arrival_airport_id=?", Airport.find_by_code(arrival_code))
+        .where("Date(date)=?", flight_date)
+        .order(:duration)
+  end
 end
