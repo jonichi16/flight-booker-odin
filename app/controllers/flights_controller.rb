2824ff38845568e54d73d97637172e5073679c5a
Jhonel Villanueva <vvjhonel@gmail.com>
2023-02-26 22:12:32 +0800

    feat: Edit index page to show the list of available airports

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,6 @@
 class FlightsController < ApplicationController
   def index
+    @airports = Airport.all.order(:name)
     @airport_options = Airport.all.order(:code).map { |airport| [ airport.code, airport.code ] }
     @flights = Flight.pick_flight(params[:departure_code], params[:arrival_code], params[:date]).includes(:departure_airport, :arrival_airport)
     @default_selected_flight = @flights.first
